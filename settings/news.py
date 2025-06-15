1708855ac1f4c52fc39795c3fb7610d0a6ec628f
deepanshujain17 <deepanshu.send@gmail.com>
2025-06-16 03:49:19 +0530

    Update categories to 9

--- settings/news.py
+++ settings/news.py
@@ -3,8 +3,8 @@ from .config import get_env_var
 
 class NewsSettings(BaseModel):
     api_key: str = Field(default=get_env_var("GNEWS_API_KEY"))
-    categories: list[str] = ["sports", "world", "nation", "business",
-                             "technology", "entertainment", "science", "health"]
+    categories: list[str] = ["general", "sports", "world", "nation", "business",
+                            "technology", "entertainment", "science", "health"]
     category_bgm: dict = {"default": "bgm_find"}
     category_bg_image: dict = {"default": "bg_image"}
     language: str = "en"
