63019cd1b8eba825e946f1cd2f40e3382f3dcb40
deepanshujain17 <deepanshu.send@gmail.com>
2025-06-15 19:50:17 +0530

    Revert "test fixing font v2"
    This reverts commit e154dc8bd43bc87cfeaae2263114ea42aa386486.

--- utils/web/pillow_card_generator.py
+++ utils/web/pillow_card_generator.py
@@ -283,7 +283,7 @@ def generate_news_card(article, output_path):
 
         # Add title with spacing
         current_y += HTMLSettings.TITLE_MARGIN_TOP
-        title_wrap_width = int((card_width - 2 * content_padding) / (title_size * 0.5))  # Adjusted wrapping factor
+        title_wrap_width = int((card_width - 10 * content_padding) / (title_size * 0.5))  # Adjusted wrapping factor
         title_lines = textwrap.wrap(title, width=title_wrap_width)
         for line in title_lines:
             # Use bold font for title text
@@ -292,7 +292,7 @@ def generate_news_card(article, output_path):
         current_y += 30  # Space after title
 
         # Add description with spacing
-        desc_wrap_width = int((card_width - 0 * content_padding) / (desc_size * 0.5))  # Adjusted wrapping factor
+        desc_wrap_width = int((card_width - 10 * content_padding) / (desc_size * 0.4))  # Adjusted wrapping factor
         desc_lines = textwrap.wrap(description, width=desc_wrap_width)
         line_spacing = int(desc_size * 0.3)
         for line in desc_lines:
